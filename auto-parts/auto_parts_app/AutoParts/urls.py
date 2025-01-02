80cd65607823d7d49cb2f5609a24b7a27aa2253a
Kobby24 <geffah005@st.ug.edu.gh>
2025-01-02 14:39:27 +0000

    Delete auto-parts/auto_parts_app directory

--- auto-parts/auto_parts_app/AutoParts/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
-    path("signup/", views.signup, name="signup")
-]
--- auto-parts/auto_parts_app/AutoParts/utils.py
+++
@@ -1,3 +0,0 @@
-with open("./models.py", "rb") as f:
-    content = f.read()
-    print(b"\x00" in content)  # Should return False
