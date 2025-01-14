2641123eb06777218346cfa2466e8401a2b4c754
Kobby24 <geffah005@st.ug.edu.gh>
2025-01-14 00:49:59 +0000

    cart functionality

--- main_app/auto_parts_app/AutoParts/static/js/head.js
+++ main_app/auto_parts_app/AutoParts/static/js/head.js
@@ -22,3 +22,32 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
+
+
+    // Select cart elements
+const cartItems = document.getElementById('cart-items');
+const cartTotal = document.getElementById('cart-total');
+const cartNum = document.getElementById('cart-count');
+
+let totalAmount = 0;
+let totalNum = 0;
+
+// Add event listener for buttons
+document.querySelectorAll('.add-to-cart-btn').forEach(button => {
+  button.addEventListener('click', event => {
+    const productElement = event.target.closest('.card');
+    const productName = productElement.querySelector('p').innerText;
+    const productPrice = parseFloat(productElement.dataset.price);
+
+    // Add item to cart display
+    const listItem = document.createElement('li');
+    listItem.textContent = `${productName} - $${productPrice.toFixed(2)}`;
+    cartItems.appendChild(listItem);
+
+    // Update total
+    totalAmount += productPrice;
+    totalNum += 1
+    cartTotal.textContent = totalAmount.toFixed(2);
+    cartNum.textContent = totalNum.toFixed(2);
+  });
+});
