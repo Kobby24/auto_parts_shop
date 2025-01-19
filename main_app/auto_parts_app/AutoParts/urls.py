32f3b33128f3064950f456f6443ab7b7530a0b5c
Kobby24 <geffah005@st.ug.edu.gh>
2025-01-19 21:53:04 +0000

    managing main

--- main_app/auto_parts_app/AutoParts/urls.py
+++ main_app/auto_parts_app/AutoParts/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
-    path("shop/<brand>/", views.main_shop, name="main_shop"),
+    path("shop/<brand>", views.main_shop, name="main_shop"),
     path("reset/", views.reset_password, name="reset_password"),
     path("pay/",views.buy,name='buy')
 ]
